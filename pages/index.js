dad8ff7821e4357fa3bbbde4249c975df764fc0b
Mario Souto <soutomarios@gmail.com>
2021-01-22 19:23:33 -0300

    m

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Imersão React</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Bem vindo a Imersão React
         </h1>
 
         <p className={styles.description}>
